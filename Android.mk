cd7aa2c0f10e06d4db3448d2a4d3b08754f889ac
Thecrazyskull <anaskarbila@gmail.com>
2017-02-10 09:38:50 -0500

    Launcher: double tap on home screen to turn off screen
    Change-Id: I554c005d5e523aca0842c78a353686e86af1a7f2

--- Android.mk
+++ Android.mk
@@ -68,7 +68,9 @@ else
   LOCAL_SDK_VERSION := system_current
   LOCAL_MIN_SDK_VERSION := 26
 endif
+<<<<<<< HEAD
 LOCAL_PACKAGE_NAME := Launcher3QuickStep
+LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3
